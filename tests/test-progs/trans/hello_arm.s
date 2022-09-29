a6aca82d6869bd4dded86d9d5b18bb4e247ef7c6
jensen-yan <yanyue171@mails.ucas.ac.cn>
2022-09-29 14:24:05 +0800

    cpu: 完成翻译整个流程！B不要提前停下，否则会关闭进程资源（cout fd）。
    Change-Id: Ic4c64cfe9a5c61ececf6a7d29a2dbec1acd51ee9

--- tests/test-progs/trans/hello_arm.s
+++ tests/test-progs/trans/hello_arm.s
@@ -2,14 +2,15 @@
 .global _start
 _start:
     add r8, r0, #1     // r8 = 1, should be 2
-    add r1, r0, #1
-    add r1, r0, #1
-    add r1, r0, #1      // cache line aligned
-
-//    add r8, r0, #1      // r8 = 1
-    add r8, r8, #48     // r8 = '1'
+    add r8, r8, #'0'     // r8 = '1'
     ldr r9, =num
     str r8, [r9]
+
+    mov r0, #1          // print hello world
+    ldr r1, =message
+    ldr r2, =len
+    mov r7, #4
+    swi 0
 
     mov r0, #1
     ldr r1, =num        // print r8
@@ -17,7 +18,7 @@ _start:
     mov r7, #4
     swi 0
 
-    mov r0, #1
+    mov r0, #1          // print hello world
     ldr r1, =message
     ldr r2, =len
     mov r7, #4
@@ -28,9 +29,10 @@ _start:
 
 .data
 message:
-    .asciz "hello world I am yanyue\n"
+    .asciz "hello world I am executor\n"
+len = .-message         // current addr - message addr
 num:
     .ascii " "
-len = .-message
+
 
 
--- tests/test-progs/trans/trans.s
+++ tests/test-progs/trans/trans.s
@@ -4,13 +4,24 @@
 
     // mov r8, #0x74
     // movt r8, #1
-    mov r8, #0x0        // add inst addr 0x0
+    mov r8, #0x400        // add inst addr 0x0
     ldr r9, [r8]        // add inst e2808001  add     r8, r0, #1
     add r9, r9, #1      // add inst e2808002  add     r8, r0, #2
     str r9, [r8]
 
-    mov r7, #1          // stop
+    mov r0, #1          // print hello world
+    ldr r1, =message
+    ldr r2, =len
+    mov r7, #4
     swi 0
+
+    mov r7, #1
+    // swi 0            // do not stop!
+
+.data
+message:
+    .asciz "hello world I am translator\n"
+len = .-message         // current addr - message addr
 
 
 
